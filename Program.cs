54f933816c8f471c7e1b642ee3770a08e6e0e42d
Radhika-222 <wandareradhika@gmail.com>
2023-11-21 16:14:45 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_PropertiesPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HerbalProduct product = new HerbalProduct();
+            product.setInfo();
+            product.Show();
+            Product product1 = new Product();
+            product1.setInfo();
+            product1.Show();
+            Console.ReadKey();
+        }
+    }
+}
+        
